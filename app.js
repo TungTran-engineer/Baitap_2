3cc712d001eb60c31941a23552db855d3f6c9d85
TungTran-engineer <tunglatoi2004@gmail.com>
2024-10-16 19:31:38 +0700

    Cập nhật

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = 3000;
 
-// Định nghĩa __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware phục vụ file tĩnh từ thư mục public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route lấy danh sách tỉnh thành từ file JSON
 app.get('/city', (req, res) => {
     const filePath = path.join(__dirname, 'vehicle_plates.json');
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -25,19 +22,19 @@ app.get('/city', (req, res) => {
     });
 });
 
-// Route lấy biển số theo tỉnh thành
+
 app.get('/plates/:city', (req, res) => {
     const city = req.params.city;
-    console.log(`Requested city: ${city}`); // Debug: log requested city
+    console.log(`Requested city: ${city}`);
     const filePath = path.join(__dirname, 'city/vehicle_plates.json');
 
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
-            console.error('Error reading file:', err); // Debug: log file reading error
+            console.error('Error reading file:', err);
             return res.status(500).json({ error: 'Error reading data' });
         }
         const plates = JSON.parse(data);
-        console.log('File data:', plates); // Debug: log data from JSON file
+        console.log('File data:', plates);
 
         const entry = plates.find(item => item.city === city);
         if (entry) {
@@ -48,7 +45,6 @@ app.get('/plates/:city', (req, res) => {
     });
 });
 
-// Khởi động server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
